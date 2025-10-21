855c099d544872571524137a70a8f54f05e64b42
SEBASTIANTORRESL0K1 <svelasco13@ucol.mx>
2025-10-21 09:55:57 -0600

    checa el objeto o la clase de cliente

--- src/models/CLIENTE.js
+++ src/models/CLIENTE.js
@@ -1,11 +1,33 @@
 import { Usuario } from "./USUARIO";
 export class Cliente extends Usuario {
-
-}
-
-export class Cliente extends Usuario {
-    constructor({ id_cliente = null, id_usuario = null, nombre, primer_apellido, segundo_apellido, fecha_nacimiento, sexo, codigo_postal, numero_telefono, correo, contrasena, puntos, id_membresia, activo }) {
-        super({ id_usuario, nombre, primer_apellido, segundo_apellido, fecha_nacimiento, sexo, codigo_postal, numero_telefono, correo, contrasena });
+    constructor({ 
+        id_cliente = null, 
+        id_usuario = null, 
+        nombre = null, 
+        primer_apellido = null, 
+        segundo_apellido = null, 
+        fecha_nacimiento = null, 
+        sexo = null, 
+        codigo_postal = null, 
+        numero_telefono = null, 
+        correo = null, 
+        contrasena = null, 
+        puntos, 
+        id_membresia, 
+        activo 
+    }) {
+        super({ 
+            id_usuario, 
+            nombre, 
+            primer_apellido, 
+            segundo_apellido, 
+            fecha_nacimiento, 
+            sexo, 
+            codigo_postal, 
+            numero_telefono, 
+            correo, 
+            contrasena 
+        });
         this.id_cliente = id_cliente;
         this.puntos = puntos;
         this.id_membresia = id_membresia;
